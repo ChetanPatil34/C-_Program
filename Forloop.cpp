bdd2bafe2e5c3d64fb89a8b25fa50ea61f7c0210
ChetanPatil34 <cp9210639@gmail.com>
2024-04-15 23:49:00 +0530

    [ADDED]: For loop

---
+++ Forloop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int counter=50;		// global variable
+
+int main()
+{
+
+	int counter;	// local variable 
+
+	for(counter=1;counter<10;counter++) 
+ 	{
+		cout<<endl<<::counter/counter;	 
+										
+	}						
+}
+				
