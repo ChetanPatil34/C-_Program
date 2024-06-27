83d4ffaaa9d5b1c5423ac77bd5725b0d6cb89cf7
ChetanPatil34 <cp9210639@gmail.com>
2024-06-27 22:35:37 +0400

    [ADDED]:CHETAN Cpp Program

---
+++ chetanpatil.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=11,b=90;
+	float c=100.5,d=10.5;
+	printf("++a=%d\n",++a);
+	printf("--b=%d\n",--b);
+	printf("++c=%f\n",++c);
+	printf("--d=%f\n",--d);
+	return 0;
+	
+}
